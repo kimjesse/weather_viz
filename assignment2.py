cda601e85ee97b6bbc48c660e6ad08bc852bdf3d
Jesse Kim <kimho@umich.edu>
2025-11-02 11:35:39 -0500

    adding source code back

--- assignment2.py
+++ assignment2.py
@@ -7,33 +7,6 @@
 get_ipython().run_cell_magic('javascript', '', 'IPython.OutputArea.prototype._should_scroll = function(lines) {\n    return false; // disable scroll bar when displaying Folium map\n}')
 
 
-# # Assignment 2
-# 
-# Before working on this assignment please read these instructions fully. In the submission area, you will notice that you can click the link to **Preview the Grading** for each step of the assignment. This is the criteria that will be used for peer grading. Please familiarize yourself with the criteria before beginning the assignment.
-# 
-# The data for this assignment comes from a subset of The National Centers for Environmental Information (NCEI) [Global Historical Climatology Network daily (GHCNd)](https://www.ncei.noaa.gov/products/land-based-station/global-historical-climatology-network-daily) (GHCN-Daily). The GHCN-Daily is comprised of daily climate records from thousands of land surface stations across the globe - it's a wonderfully large dataset to play with! In particular, you will be asked to use data from the Ann Arbor Michigan location (my home!). and this is stored in the file: `assets/fb441e62df2d58994928907a91895ec62c2c42e6cd075c2700843b89.csv`
-# 
-# Each row in this datafile corresponds to a single observation from a weather station, and has the following variables:
-# * **id** : station identification code
-# * **date** : date in YYYY-MM-DD format (e.g. 2012-01-24 = January 24, 2012)
-# * **element** : indicator of element type
-#     * TMAX : Maximum temperature (tenths of degrees C)
-#     * TMIN : Minimum temperature (tenths of degrees C)
-# * **value** : data value for element (tenths of degrees C)
-# 
-# For this assignment, you must:
-# 
-# 1. Read the documentation and familiarize yourself with the dataset, then write a python notebook which plots line graphs of the record high and record low temperatures by day of the year over the period 2005-2014. The area between the record high and record low temperatures for each day should be shaded.
-# 2. Overlay a scatter of the 2015 data for any points (highs and lows) for which the ten year record (2005-2014) record high or record low was broken in 2015. (Based on the graph, do you think extreme weather is getting more frequent in 2015?)
-# 3. Watch out for leap days (i.e. February 29th), it is reasonable to remove these points from the dataset for the purpose of this visualization.
-# 4. Make the visual nice! Leverage principles from the first module in this course when developing your solution. Consider issues such as legends, labels, and chart junk.
-# 
-# I've written some steps I think would be good to go through, but there are other ways to solve this assignment so feel free to explore the pandas library! What I really want to see is an image that looks like this sketch I drew at my desk:
-# 
-# ![](assets/chris_sketch.png)
-
-# In[2]:
-
 
 #  I'll be using the folium package to render the data into a map in Jupyter.
 
